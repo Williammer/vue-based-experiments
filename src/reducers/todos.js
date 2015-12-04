430159ea42a2ccba9b4793506c960bed972132a1
Williammer <supervegeter@gmail.com>
2015-12-04 08:36:21 +0800

    added todo next

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -2,6 +2,7 @@ export default function todos(state = [], action) {
   
   switch (action.type) {
     case 'ADD_TODO':
+      // TODO: use Object.assign({}, state, {...}) to keep orig state untouched.
       return state.concat([{ todo: action.todo, checked: false }])
     
     case 'TOGGLE_TODO':
